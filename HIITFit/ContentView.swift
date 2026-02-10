155706af50485a18516ce0598ca564e56c4e3e64
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-10 23:29:14 +0100

    refactor(main): Remove tab item
    There will be no use of these items, because I changed tab view style to page tab view style

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            Text("Welcome").tabItem { Text("Welcome") }
-            Text("Execrice 1").tabItem { Text("Execrice 1") }
-            Text("Execrice 2").tabItem { Text("Execrice 2") }
+            Text("Welcome")
+            Text("Execrice 1")
+            Text("Execrice 2")
         }.tabViewStyle(PageTabViewStyle())
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
