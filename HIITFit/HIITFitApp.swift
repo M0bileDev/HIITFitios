4965d603bd09079c234678ced7a3208f3362dd84
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-10 22:59:44 +0100

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Damian Ogórek on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
