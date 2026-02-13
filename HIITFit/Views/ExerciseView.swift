8c7d5d59873314b56df5697fb9b71e8d078b7fe3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-13 22:38:26 +0100

    feature(views): Extend exercise view by adding text components

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -13,7 +13,14 @@ struct ExerciseView: View {
     let index: Int
     
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            Text(exerciseNames[index])
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        }
     }
 }
 
