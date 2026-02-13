79c38ebc3c68567efbaa55bd7c225de167cead6c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-13 23:49:38 +0100

    refactor(views): extract view

---
+++ HIITFit/Views/HeaderView.swift
@@ -0,0 +1,10 @@
+VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                }.font(.title2)
+            }
