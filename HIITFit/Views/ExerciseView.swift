eb47e53e478245c028f9b8819f0eb14a992d2976
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-15 00:34:59 +0100

    refactor(views): code restructure

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -13,31 +13,17 @@ struct ExerciseView: View {
     var exercise: Exercise {
         Exercise.exercises[index]
     }
-    // alias for Double
-    let interval: TimeInterval = 30
+    
     var body: some View {
         // container view that provides with the screen’s measurements
         GeometryReader { geometry in
             VStack {
                 HeaderView(exerciceName: exercise.exerciseName).padding(.bottom)
-                if let url = Bundle.main.url(
-                    forResource: exercise.videoName,
-                    withExtension: "mp4"
-                ) {
-                    VideoPlayer(player: AVPlayer(url: url)).frame(
-                        height: geometry.size.height * 0.45
-                    )
-                } else {
-                    Text("Couldn't find \(exercise.videoName).mp4")
-                        .foregroundColor(
-                            .red
-                        )
-                }
-                Text(
-                    //current date and time -> adds interval seconds to this value
-                    Date().addingTimeInterval(interval),
-                    style: .timer
-                ).font(.system(size: geometry.size.height * 0.07))
+                VideoPlayerView(
+                    videoName: exercise.videoName,
+                    videExtension: "mp4"
+                ).frame(height: geometry.size.height * 0.45)
+                TimerView(fontSize: geometry.size.height * 0.07)
                 Button("Start/Done") {}.font(.title3).padding()
                 RatingView().padding()
                 Spacer()
