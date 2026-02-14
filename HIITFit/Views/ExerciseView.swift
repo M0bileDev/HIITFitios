1cfdef6b180a5a2289f9e0821eebbf802dc26d80
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-14 23:35:05 +0100

    feature(views): change  size of video player
    Changed full screen size to 45% of view height

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -15,22 +15,25 @@ struct ExerciseView: View {
     }
 
     var body: some View {
-        VStack {
-            HeaderView(exerciceName: exercise.exerciseName)
-            if let url = Bundle.main.url(
-                forResource: exercise.videoName,
-                withExtension: "mp4"
-            ) {
-                VideoPlayer(player: AVPlayer(url: url))
-            } else {
-                Text("Couldn't find \(exercise.videoName).mp4").foregroundColor(
-                    .red
-                )
+        // container view that provides with the screen’s measurements
+        GeometryReader { geometry in
+            VStack {
+                HeaderView(exerciceName: exercise.exerciseName)
+                if let url = Bundle.main.url(
+                    forResource: exercise.videoName,
+                    withExtension: "mp4"
+                ) {
+                    VideoPlayer(player: AVPlayer(url: url)).frame(height: geometry.size.height * 0.45)
+                } else {
+                    Text("Couldn't find \(exercise.videoName).mp4").foregroundColor(
+                        .red
+                    )
+                }
+                Text("Timer")
+                Text("Start/Done button")
+                Text("Rating")
+                Text("History button")
             }
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
         }
     }
 }
