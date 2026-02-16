777f959edab89aa5842dbe916148e43747e66d1d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-16 22:12:29 +0100

    feature(views): provide form with two sections

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -11,13 +11,33 @@ struct HistoryView: View {
     let today = Date()
     //time interval of two days
     let yesterday = Date().addingTimeInterval(-86400)
-    
+
     let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let exercises2 = ["Squat", "Step Up", "Burpee"]
-    
+
     var body: some View {
-        VStack{
+        VStack {
             Text("History").font(.title).padding()
+            Form {
+                Section(
+                    content: {},
+                    header: {
+                        Text(
+                            today.formatted(.dateTime.month(.abbreviated).day())
+                        ).font(.headline)
+                    }
+                )
+                Section(
+                    content: {},
+                    header: {
+                        Text(
+                            yesterday.formatted(
+                                .dateTime.month(.abbreviated).day()
+                            )
+                        ).font(.headline)
+                    }
+                )
+            }
         }
     }
 }
