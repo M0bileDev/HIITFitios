fd62732614a3f83a5b30790c220c2655491fb5dd
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-17 21:26:45 +0100

    feature(model): provide history store model

--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct ExerciseDay: Identifiable {
+    let id = UUID()
+    let date: Date
+    var exercises: [String] = []
+}
+
+struct HistoryStore {
+    var exerciseDay: [ExerciseDay] = []
+}
