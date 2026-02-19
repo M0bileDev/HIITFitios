ddfac8095a4d835b13f72f8e10aa4a407036dc6a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-19 21:54:44 +0100

    feature(views): provide ZStack
    Added button and set it's position to the top right corner

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -11,27 +11,38 @@ struct HistoryView: View {
     let history = HistoryStore()
 
     var body: some View {
-        VStack {
-            Text("History").font(.title).padding()
-            Form {
-                ForEach(
-                    history.exerciseDays,
-                    content: { day in
-                        Section(
-                            header: Text(day.date.formatted(from: "dd MMM")),
-                            content: {
-                                ForEach(
-                                    day.exercises,
-                                    id: \.self,
-                                    content: { exercise in
-                                        Text(exercise)
-                                    }
-                                )
-                            }
-                        )
-                    }
-                )
+        ZStack(alignment: .topTrailing) {
+            Button(
+                action: {},
+                label: {
+                    Image(systemName: "xmark.circle")
+                }
+            ).font(.title).padding(.trailing)
+            VStack {
+                Text("History").font(.title).padding()
+                Form {
+                    ForEach(
+                        history.exerciseDays,
+                        content: { day in
+                            Section(
+                                header: Text(
+                                    day.date.formatted(from: "dd MMM")
+                                ),
+                                content: {
+                                    ForEach(
+                                        day.exercises,
+                                        id: \.self,
+                                        content: { exercise in
+                                            Text(exercise)
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                    )
+                }
             }
+
         }
     }
 }
