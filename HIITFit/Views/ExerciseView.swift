d3b14eca006581297dd38e88332efd9ca478a3d2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-19 22:02:18 +0100

    refactor(views): rename property

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -18,7 +18,7 @@ struct ExerciseView: View {
         // container view that provides with the screen’s measurements
         GeometryReader { geometry in
             VStack {
-                HeaderView(exerciceName: exercise.exerciseName).padding(.bottom)
+                HeaderView(titleText: exercise.exerciseName).padding(.bottom)
                 VideoPlayerView(
                     videoName: exercise.videoName,
                     videExtension: "mp4"
