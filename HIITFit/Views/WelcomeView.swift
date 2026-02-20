b4c5411a00f7b983e0954c912752cb614143563a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-20 22:51:57 +0100

    refactor(views): replace modifiers with custom modifier

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -22,9 +22,7 @@ struct WelcomeView: View {
                         Text("with hight intensity interval training").font(.headline)
                     }
                     Image("step-up")
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(width: 240, height: 240)
+                        .resizedToFill(width: 240, height: 240)
                         .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 }
             }
