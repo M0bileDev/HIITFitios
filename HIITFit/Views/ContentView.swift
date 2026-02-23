0978de783351c745c7299c39718218404b99e48f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-23 21:02:23 +0100

    feature(views): provide @State property wrapper
    Created source of truth, passed property value inside other views

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedTab = 9
+
     var body: some View {
-        TabView {
-            WelcomeView()
+        TabView(selection: $selectedTab) {
+            WelcomeView(selectedTab: $selectedTab).tag(9)
             ForEach(
-                // range is not longer fixed, need to provide id 
+                // range is not longer fixed, need to provide id
                 Exercise.exercises.indices,
                 // \.self -> each element it is own identifier
                 id: \.self,
                 content: { index in
-                    ExerciseView(index: index)
+                    ExerciseView(selectedTab: $selectedTab, index: index).tag(
+                        index
+                    )
                 }
             )
             Text("Execrice 2")
