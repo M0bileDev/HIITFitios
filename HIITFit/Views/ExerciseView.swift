29acd9d5838aada41869d20fd58d74158eeceae2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-24 19:15:39 +0100

    feature(views): support sheet size with two values: medium and large

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -51,7 +51,7 @@ struct ExerciseView: View {
                 HStack(spacing: 150) {
                     startButton
                     doneButton.sheet(isPresented: $showSuccess, content: {
-                        SuccessView()
+                        SuccessView().presentationDetents([.medium, .large])
                     })
                 }.font(.title3).padding()
                 RatingView(rating: $rating).padding()
