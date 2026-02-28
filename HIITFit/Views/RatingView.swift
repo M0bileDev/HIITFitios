8b069b7cfffbca10dc65a6f1b00173db17ea2dc6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-28 20:34:25 +0100

    feature(views): provide logic that reads rating from UserDefaults and apply it to private state

--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct RatingView: View {
-    
+
     let exerciseIndex: Int
     @AppStorage("ratings") private var ratings = "0000"
     @State private var rating = 0
-    
+
     let maximumRating = 5
     let onColor = Color.red
     let offColor = Color.gray
-    
+
     var body: some View {
         HStack {
             ForEach(
@@ -25,7 +25,15 @@ struct RatingView: View {
                 content: { index in
                     Image(systemName: "waveform.path.ecg").foregroundColor(
                         index > rating ? offColor : onColor
-                    )
+                    ).onAppear(perform: {
+                        let index = ratings.index(
+                            ratings.startIndex,
+                            offsetBy: exerciseIndex
+                        )
+                        let character = ratings[index]
+                        rating = character.wholeNumberValue ?? 0
+
+                    })
                     .onTapGesture {
                         rating = index
                     }
