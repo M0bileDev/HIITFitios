3d3ede19a110cd12098d295f3808c14a81275548
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-28 20:17:58 +0100

    feature(views): cretae function that prints app's document directory folder
    There are three directories provided by app (Documents, Library and Library/Caches)

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -12,6 +12,9 @@ struct HIITFitApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: {
+                    print(URL.documentsDirectory)
+                })
         }
     }
 }
