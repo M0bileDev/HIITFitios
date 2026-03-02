8cbecbedea7790a1347302d97998f0a2766e47fe
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-02 22:25:46 +0100

    feature(views): create state object property inside app view
    Assigned state object as environmentObject for the content view

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct HIITFitApp: App {
+
+    @StateObject private var historyStore = HistoryStore()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onAppear(perform: {
                     print(URL.documentsDirectory)
-                })
+                }).environmentObject(historyStore)
         }
     }
 }
