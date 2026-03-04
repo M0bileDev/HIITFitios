c121ceec44c25a7c521ae6248179a4e8decea1a2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-04 19:44:38 +0100

    feature(model): provide logic to load data from plist file into the history model (publisher)

--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -79,13 +79,29 @@ class HistoryStore: ObservableObject {
             throw FileError.saveFailure
         }
     }
+
+    func load() throws {
+        do {
+            let data = try Data(contentsOf: dataURL)
+            let plistData = try PropertyListSerialization.propertyList(
+                from: data,
+                options: [],
+                format: nil
+            )
+            let convertedPlistData = plistData as? [[Any]] ?? []
+            exerciseDays = convertedPlistData.map {
+                ExerciseDay(
+                    date: $0[1] as? Date ?? Date(),
+                    exercises: $0[2] as? [String] ?? []
+                )
+            }
+        } catch {
+            throw FileError.loadFailure
+        }
+    }
 }
 
 enum FileError: Error {
     case loadFailure
     case saveFailure
-}
-
-func load() throws {
-    //    throw FileError.loadFailure
 }
