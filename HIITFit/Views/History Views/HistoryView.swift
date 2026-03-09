33e0f92ce9f75a304f277cb5aa6b87ca9099c886
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-09 20:54:27 +0100

    refactor(views): change view destination

---
+++ HIITFit/Views/History Views/HistoryView.swift
@@ -0,0 +1,61 @@
+//
+//  HistoryVIew.swift
+//  HIITFit
+//
+//  Created by Damian Ogórek on 16/02/2026.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+  @Binding var showHistory: Bool
+  @EnvironmentObject var history: HistoryStore
+
+  var headerView: some View {
+    HStack {
+      Spacer()
+      Text("History")
+        .font(.title)
+      Spacer()
+      Button {
+        showHistory.toggle()
+      } label: {
+        Image(systemName: "xmark.circle")
+      }
+      .font(.title)
+    }
+  }
+
+  func dayView(day: ExerciseDay) -> some View {
+    Section(
+      header:
+        Text(day.date.formatted(from: "MMM d"))
+        .font(.headline)) {
+          exerciseView(day: day)
+    }
+  }
+
+  func exerciseView(day: ExerciseDay) -> some View {
+    ForEach(day.exercises, id: \.self) { exercise in
+      Text(exercise)
+    }
+  }
+
+  var body: some View {
+    VStack {
+      headerView
+        .padding()
+      Form {
+        ForEach(history.exerciseDays) { day in
+          dayView(day: day)
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  let history = HistoryStore(preview: true)
+  HistoryView(showHistory: .constant(true))
+    .environmentObject(history)
+}
