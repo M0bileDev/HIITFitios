863847df1e393259347d92cde1ba487f61b891f3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-10 22:08:41 +0100

    feature(views): add DisclosureGroup to provide parent-child view relation (expand parent view to list the children)

--- HIITFit/Views/History Views/HistoryView.swift
+++ HIITFit/Views/History Views/HistoryView.swift
@@ -27,8 +27,12 @@ struct HistoryView: View {
     }
 
     func dayView(day: ExerciseDay) -> some View {
-        Text(day.date.formatted(from: "d MMM YYYY"))
-            .font(.headline)
+        DisclosureGroup {
+            exerciseView(day: day)
+        } label: {
+            Text(day.date.formatted(from: "d MMM YYYY"))
+                .font(.headline)
+        }
     }
 
     func exerciseView(day: ExerciseDay) -> some View {
