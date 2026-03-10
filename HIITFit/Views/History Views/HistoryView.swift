699c3315cc2eda202d69f50e163d3feb9451461f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-10 22:02:30 +0100

    refactor(views): replace section view with List, enable swipe to delete action (build-in list feature)

--- HIITFit/Views/History Views/HistoryView.swift
+++ HIITFit/Views/History Views/HistoryView.swift
@@ -8,55 +8,52 @@
 import SwiftUI
 
 struct HistoryView: View {
-  @Binding var showHistory: Bool
-  @EnvironmentObject var history: HistoryStore
+    @Binding var showHistory: Bool
+    @EnvironmentObject var history: HistoryStore
 
-  var headerView: some View {
-    HStack {
-      Spacer()
-      Text("History")
-        .font(.title)
-      Spacer()
-      Button {
-        showHistory.toggle()
-      } label: {
-        Image(systemName: "xmark.circle")
-      }
-      .font(.title)
-    }
-  }
-
-  func dayView(day: ExerciseDay) -> some View {
-    Section(
-      header:
-        Text(day.date.formatted(from: "MMM d"))
-        .font(.headline)) {
-          exerciseView(day: day)
-    }
-  }
-
-  func exerciseView(day: ExerciseDay) -> some View {
-    ForEach(day.uniqueExercises, id: \.self) { exercise in
-      Text(exercise)
-            .badge(day.countExercise(exercise: exercise))
-    }
-  }
-
-  var body: some View {
-    VStack {
-      headerView
-        .padding()
-      Form {
-        ForEach(history.exerciseDays) { day in
-          dayView(day: day)
+    var headerView: some View {
+        HStack {
+            Spacer()
+            Text("History")
+                .font(.title)
+            Spacer()
+            Button {
+                showHistory.toggle()
+            } label: {
+                Image(systemName: "xmark.circle")
+            }
+            .font(.title)
         }
-      }
     }
-  }
+
+    func dayView(day: ExerciseDay) -> some View {
+        Text(day.date.formatted(from: "d MMM YYYY"))
+            .font(.headline)
+    }
+
+    func exerciseView(day: ExerciseDay) -> some View {
+        ForEach(day.uniqueExercises, id: \.self) { exercise in
+            Text(exercise)
+                .badge(day.countExercise(exercise: exercise))
+        }
+    }
+
+    var body: some View {
+        VStack {
+            headerView
+                .padding()
+            List($history.exerciseDays, editActions: [.delete]) { $day in
+                dayView(day: day)
+            }
+        }
+        .onDisappear {
+            try? history.save()
+        }
+    }
 }
 
 #Preview {
-  let history = HistoryStore(preview: true)
-  HistoryView(showHistory: .constant(true))
-    .environmentObject(history)
+    let history = HistoryStore(preview: true)
+    HistoryView(showHistory: .constant(true))
+        .environmentObject(history)
 }
