7858f3f841fad0dcdce5b047238a2a5911e3042e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-15 20:19:35 +0100

    feature(history): create view that displays data for a week of exercices

---
+++ HIITFit/Views/History Views/BarChartWeekView.swift
@@ -0,0 +1,41 @@
+//
+//  BarChartWeekView.swift
+//  HIITFit
+//
+//  Created by Damian Ogórek on 15/03/2026.
+//
+
+import Charts
+import SwiftUI
+
+struct BarChartWeekView: View {
+
+    @EnvironmentObject var historyStore: HistoryStore
+    @State private var weekData: [ExerciseDay] = []
+
+    var body: some View {
+        Chart(
+            weekData,
+            content: { day in
+                BarMark(
+                    x: .value("Date", day.date, unit: .day),
+                    y: .value("Total count", day.exercises.count)
+                )
+                RuleMark(y: .value("Exercices per day", 4))
+                    .foregroundStyle(.red)
+            }
+        ).onAppear(perform: {
+            let firstDate = historyStore.exerciseDays.first?.date ?? Date()
+            let previousWeek = firstDate.previousSevenDays
+            weekData = previousWeek.map { date in
+                historyStore.exerciseDays.first(where: {
+                    $0.date.isSameDay(from: date)
+                }) ?? ExerciseDay(date: date)
+            }
+        })
+    }
+}
+
+#Preview {
+    BarChartWeekView().environmentObject(HistoryStore(preview: true))
+}
